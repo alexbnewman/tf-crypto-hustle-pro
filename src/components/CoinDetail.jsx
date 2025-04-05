6786d44d6ba485699e1f33ade54f9bba3973ed68
Alex Newman <alex02new@outlook.com>
2025-04-05 13:13:01 -0400

    editing coindetail table

--- src/components/CoinDetail.jsx
+++ src/components/CoinDetail.jsx
@@ -46,61 +46,70 @@ const CoinDetail = () => {
             {fullDetails.textData[params.symbol].Algorithm}{" "}
             </div>
             <table>
-            <tbody> 
-                <tr>
-                <th>Launch Date </th>
-                <td> </td>
-                </tr>
-                <tr>
-                <th>Website </th>
-                <td> </td>
-                </tr>
-                <tr>
-                <th>Whitepaper </th>
-                <td> </td>
-                </tr>
-                <tr>
-                <th>Monetary Symbol </th>
-                <td> </td>
-                </tr>
-                <tr>
-                <th>Market </th>
-                <td> </td>
-                </tr>
-                <tr>
-                <th>Last Transaction </th>
-                <td> </td>
-                </tr>
-                <tr>
+            <tbody>
+              <tr>
+                <th>Launch Date</th>
+                <td>{fullDetails.textData[params.symbol].AssetLaunchDate || "N/A"}</td>
+              </tr>
+              <tr>
+                <th>Website</th>
+                <td>
+                  <a href={fullDetails.textData[params.symbol].WebsiteUrl} target="_blank" rel="noopener noreferrer">
+                    {fullDetails.textData[params.symbol].WebsiteUrl || "N/A"}
+                  </a>
+                </td>
+              </tr>
+              <tr>
+                <th>Whitepaper</th>
+                <td>
+                  <a href={fullDetails.textData[params.symbol].WhitePaper?.Url} target="_blank" rel="noopener noreferrer">
+                    {fullDetails.textData[params.symbol].WhitePaper?.Url || "N/A"}
+                  </a>
+                </td>
+              </tr>
+              <tr>
+                <th>Monetary Symbol</th>
+                <td>{params.symbol}</td>
+              </tr>
+              <tr>
+                <th>Market</th>
+                <td>{fullDetails.numbers[params.symbol].USD.MARKET || "N/A"}</td>
+              </tr>
+              <tr>
+                <th>Last Transaction</th>
+                <td>{fullDetails.numbers[params.symbol].USD.LASTUPDATE || "N/A"}</td>
+              </tr>
+              <tr>
                 <th>Last Transaction Value</th>
-                <td> </td>
-                </tr>
-                <tr>
-                <th>Volume </th>
-                <td> </td>
-                </tr>
-                <tr>
-                <th>Today's Open Price </th>
-                <td> </td>
-                </tr>
-                <tr>
-                <th>Highest Price during the Day </th>
-                <td> </td>
-                </tr>
-                <tr>
-                <th>Lowest Price during the Day </th>
-                <td> </td>
-                </tr>
-                <tr>
-                <th>Change from Previous Day </th>
-                <td> </td>
-                </tr>
-                <tr>
-                <th>Market Cap </th>
-                <td> </td>
-                </tr>
+                <td>{fullDetails.numbers[params.symbol].USD.PRICE || "N/A"}</td>
+              </tr>
+              <tr>
+                <th>Volume (24h)</th>
+                <td>{fullDetails.numbers[params.symbol].USD.VOLUME24HOUR || "N/A"}</td>
+              </tr>
+              <tr>
+                <th>Today's Open Price</th>
+                <td>{fullDetails.numbers[params.symbol].USD.OPEN24HOUR || "N/A"}</td>
+              </tr>
+              <tr>
+                <th>Highest Price during the Day</th>
+                <td>{fullDetails.numbers[params.symbol].USD.HIGH24HOUR || "N/A"}</td>
+              </tr>
+              <tr>
+                <th>Lowest Price during the Day</th>
+                <td>{fullDetails.numbers[params.symbol].USD.LOW24HOUR || "N/A"}</td>
+              </tr>
+              <tr>
+                <th>Change from Previous Day</th>
+                <td>{fullDetails.numbers[params.symbol].USD.CHANGE24HOUR || "N/A"}</td>
+              </tr>
+              <tr>
+                <th>Market Cap</th>
+                <td>{fullDetails.numbers[params.symbol].USD.MKTCAP || "N/A"}</td>
+              </tr>
             </tbody>
-            </table>
+          </table>
+
         </div>
     );
   };
